11719780c9b044043183b5088c163e55cb492db6
quantu99 <callmequantu@gmail.com>
2023-05-20 19:36:19 +0700

    deploy website

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import Home from '../pages/Home';
 import DetailPlace from '../pages/DetailPlace/DetailPlace';
 import Response from '../pages/Response/Response';
 const publicRoutes = [
-    { path: '/', component: Home, layout: null },
+    { path: '/travel-ui', component: Home, layout: null },
     { path: '/:placeId', component: DetailPlace, layout: null },
     { path: '/response', component: Response, layout: null },
 ];
